5ecbc9920ead56c7b3a9d4e1f031363f9c94c32b
Tim Watson <watson.timothy@gmail.com>
2011-03-31 21:15:10 +0100

    Enhance reltool config generation to include custom deps

--- dxkit/ebin/dxkit.app
+++ dxkit/ebin/dxkit.app
@@ -3,7 +3,7 @@
  [{description,"Diagnostic and Monitoring Toolkit"},
   {vsn,"0.0.1"},
   {mod,{dxkit_app,[]}},
-  {modules,[dxkit_app, dxkit_utils, dxkit_world_server, dxkit_net]},
+  {modules,[dxkit_utils, dxkit_world_server, dxkit_net]},
   {registered,[]},
   {applications,[kernel,stdlib]},
   {env,[]}]}.
--- setenv.sh
+++ setenv.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env sh
 
-# set up the ERL_LIBS environment variable so that this project can be built from the top level 
+# set up the ERL_LIBS environment variable so that this project can be built from the top level
 # as a cohesive whole
 
 export PWD=`pwd`
 export DEPS_DIR=`cat rebar.config | grep deps_dir | awk ' /.*/ { gsub("\\\[\"", ""); gsub("\"\\]\\}.", ""); print $2; }'`
-export ERL_LIBS="$PWD:$PWD/$DEPS_DIR:$ERL_LIBS"
+#export ERL_LIBS="$PWD:$PWD/$DEPS_DIR:$ERL_LIBS"
+export ERL_LIBS="$PWD:$ERL_LIBS"
